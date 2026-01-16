f145596164f0b76e76ac84713d0e96d632cec233
ankittxyadav <ankitxydv26@gmail.com>
2026-01-17 00:03:25 +0530

    feat: Set up initial project infrastructure including backend API, frontend constants, and Vercel deployment configuration.

--- internship-job-management/backend/index.js
+++ internship-job-management/backend/index.js
@@ -12,29 +12,20 @@ dotenv.config({});
 
 const app = express();
 
+const corsOptions = {
+    origin: ['https://internship-job-management-system.vercel.app', 'http://localhost:5173', 'http://localhost:8000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+};
+
+app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const allowedOrigins = [
-    'https://internship-job-management-system.vercel.app/api/v1gi',
-    'http://localhost:8000', // For local development testing
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
 
 const PORT = process.env.PORT || 3000;
 
